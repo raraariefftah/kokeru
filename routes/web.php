5787efcd033d05d967d0005be85e1cdd90bd4051
raraariefftah <raraariefftah07@gmail.com>
2020-11-25 23:40:19 +0700

    Awal halaman

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/c', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('awal');
 });
