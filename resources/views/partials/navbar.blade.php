3747a98b91b7cd40ccfdef6a56d4bf8c1cb6bccc
rochmad <rochmad.wa@gmail.com>
2020-12-09 21:46:54 +0700

    Menambah nama dan logout di navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,8 +4,8 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 large">Nama</span>
+               aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 large">{{ Auth::user()->nama }}</span>
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -14,12 +14,53 @@
                     Edit Profile
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal"
+                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
+
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
             </div>
         </li>
+{{--        <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--            <!-- Left Side Of Navbar -->--}}
+{{--            <ul class="navbar-nav mr-auto">--}}
+
+{{--            </ul>--}}
+
+{{--            <!-- Right Side Of Navbar -->--}}
+{{--            <ul class="navbar-nav ml-auto">--}}
+{{--                <!-- Authentication Links -->--}}
+{{--                @guest--}}
+{{--                    <li class="nav-item">--}}
+{{--                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
+{{--                    </li>--}}
+{{--                @else--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"--}}
+{{--                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                            {{ Auth::user()->nama }}--}}
+{{--                        </a>--}}
+
+{{--                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+{{--                            <a class="dropdown-item" href="{{ route('logout') }}"--}}
+{{--                               onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">--}}
+{{--                                {{ __('Logout') }}--}}
+{{--                            </a>--}}
+
+{{--                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
+{{--                                @csrf--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                @endguest--}}
+{{--            </ul>--}}
+{{--        </div>--}}
     </ul>
 </nav>
 <!-- End of Topbar -->
