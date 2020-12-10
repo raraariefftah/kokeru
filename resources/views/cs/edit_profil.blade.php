5a7bc69bcfa848abbc08fea0e9f401105a8a3e94
rochmad <rochmad.wa@gmail.com>
2020-12-11 06:44:49 +0700

    revisi edit profil manager

--- resources/views/cs/edit_profil.blade.php
+++ resources/views/cs/edit_profil.blade.php
@@ -29,7 +29,7 @@
                 <div class="alert alert-success">
                     {{ session('success') }}
                 </div>
-        @elseif(session('failed'))
+            @elseif(session('failed'))
                 <div class="alert alert-danger">
                     {{ session('failed') }}
                 </div>
